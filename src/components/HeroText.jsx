f702cf6d57f91663eee24855d269037ec799a032
suraj <surajankola9@gmail.com>
2026-02-27 15:37:43 +0530

    title changed

--- src/components/HeroText.jsx
+++ src/components/HeroText.jsx
@@ -28,7 +28,7 @@ const HeroText = () => {
             animate="visible"
             transition={{ delay: 1.2 }}
           >
-            A Developer <br /> Dedicated to Crafting
+            A data analyst <br /> Dedicated to Crafting
           </motion.p>
           <motion.div
             variants={variants}
